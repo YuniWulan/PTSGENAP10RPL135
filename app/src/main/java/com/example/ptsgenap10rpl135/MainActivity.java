195c838f5463da0949b7ef9684d3115376556b72
YuniWulan <keepcalm.upnormal@gmail.com>
2021-03-20 23:53:56 +0700

    PTSGENAP10RPL135

---
+++ app/src/main/java/com/example/ptsgenap10rpl135/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.ptsgenap10rpl135;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button login;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Email = email.getText().toString();
+                String Password = password.getText().toString();
+
+                if (Email.equalsIgnoreCase("admin@gmail.com")&& Password.equalsIgnoreCase("12345")){
+                    Toast.makeText(MainActivity.this, "Sukses Login", Toast.LENGTH_SHORT).show();
+                    Intent Intent = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(Intent);
+            }else {
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+        };
+        });
+        };
+};
+
